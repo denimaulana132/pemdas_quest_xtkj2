5f24c5711bec25c8ad5a458045e2afd48a141806
deni <111479855+denimaulana132@users.noreply.github.com>
2022-08-19 00:58:43 -0700

    Add files via upload

---
+++ python3-deni.py
@@ -0,0 +1,15 @@
+n=input("masukan nama depan: ")
+b=input("masukan nama belakang: ")
+print("nama lengkap anda adalah",n,b)
+
+a=int(input("masukan nilai A: "))
+b=float(input("masukan nilai B: "))
+c=a+b
+
+print("A=", a)
+print("B=", b)
+print(a,"+",b,"=" ,c)
+
+x= int(input("masukan suhu celcius: "))
+y=(9/5 * x) + 32
+print(x,"celcius =",y,"fahrenhait")
