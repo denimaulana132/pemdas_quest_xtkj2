4b2cc8310b03abd446a61741580b87e6dba93652
deni <111479855+denimaulana132@users.noreply.github.com>
2023-02-13 09:45:57 +0700

    Add files via upload

---
+++ deni_xtkj2/latihan_fungsi.py
@@ -0,0 +1,25 @@
+def salam():
+    print("assalamu'alaikum !! akhi ukhty")
+
+salam()
+salam()
+salam()
+
+def welcom():
+    print("selamat datang")
+welcom()
+welcom()
+welcom() 
+
+def say_hi(nama):
+    print("halo perkenalkan nama saya",nama)
+
+say_hi("deni")
+say_hi("shalman")
+
+def welcome(nama,umur,kelas):
+    print("halo nama saya:",nama)
+    print("umur:",umur)
+    print("kelas:",kelas)
+
+welcome("deni","17","10")
